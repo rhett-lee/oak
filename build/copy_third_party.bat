68c3a3b7b518509d92ceb9db4fb078bae3e46ccf
rhett-lee <rhettli@hotmail.com>
2023-03-28 20:33:59 +0800

    添加复制脚本：复制NIM_Duilib_Framework库中的.h文件和lib文件。

---
+++ build/copy_third_party.bat
@@ -0,0 +1,18 @@
+CD /d %~dp0
+SET CURRENT_BAT_PATH=%~dp0
+
+SET NIM_Duilib_Framework_SRC_PATH=%CURRENT_BAT_PATH%..\..\NIM_Duilib_Framework
+SET NIM_Duilib_Framework_DEST_PATH=%CURRENT_BAT_PATH%..\third_party\NIM_Duilib_Framework
+
+IF NOT EXIST "%NIM_Duilib_Framework_DEST_PATH%" (
+    MD "%NIM_Duilib_Framework_DEST_PATH%" 
+)
+
+XCOPY /S /Y /EXCLUDE:exclude_files.txt "%NIM_Duilib_Framework_SRC_PATH%\base\*.h"          "%NIM_Duilib_Framework_DEST_PATH%\include\base\"
+XCOPY /S /Y /EXCLUDE:exclude_files.txt "%NIM_Duilib_Framework_SRC_PATH%\duilib\*.h"        "%NIM_Duilib_Framework_DEST_PATH%\include\duilib\"
+XCOPY /S /Y /EXCLUDE:exclude_files.txt "%NIM_Duilib_Framework_SRC_PATH%\ui_components\*.h" "%NIM_Duilib_Framework_DEST_PATH%\include\ui_components\"
+
+XCOPY /S /Y "%NIM_Duilib_Framework_SRC_PATH%\libs\*.lib" "%NIM_Duilib_Framework_DEST_PATH%\libs\"
+XCOPY /S /Y "%NIM_Duilib_Framework_SRC_PATH%\libs\*.pdb" "%NIM_Duilib_Framework_DEST_PATH%\libs\"
+
+pause
