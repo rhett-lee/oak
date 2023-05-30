b036cc792a3a639539472f3b40a530aef41b128c
rhett-lee <rhettli@hotmail.com>
2023-05-30 20:17:31 +0800

    完善bat脚本。

--- build/copy_third_party.bat
+++ build/copy_third_party.bat
@@ -11,6 +11,7 @@ IF NOT EXIST "%NIM_Duilib_Framework_DEST_PATH%" (
 XCOPY /S /Y /EXCLUDE:exclude_files.txt "%NIM_Duilib_Framework_SRC_PATH%\base\*.h"          "%NIM_Duilib_Framework_DEST_PATH%\include\base\"
 XCOPY /S /Y /EXCLUDE:exclude_files.txt "%NIM_Duilib_Framework_SRC_PATH%\duilib\*.h"        "%NIM_Duilib_Framework_DEST_PATH%\include\duilib\"
 XCOPY /S /Y /EXCLUDE:exclude_files.txt "%NIM_Duilib_Framework_SRC_PATH%\ui_components\*.h" "%NIM_Duilib_Framework_DEST_PATH%\include\ui_components\"
+XCOPY /S /Y /EXCLUDE:exclude_files.txt "%NIM_Duilib_Framework_SRC_PATH%\third_party\*.h"   "%NIM_Duilib_Framework_DEST_PATH%\include\third_party\"
 
 XCOPY /S /Y "%NIM_Duilib_Framework_SRC_PATH%\libs\*.lib" "%NIM_Duilib_Framework_DEST_PATH%\libs\"
 XCOPY /S /Y "%NIM_Duilib_Framework_SRC_PATH%\libs\*.pdb" "%NIM_Duilib_Framework_DEST_PATH%\libs\"
